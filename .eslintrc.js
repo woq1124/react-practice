8590fd2ec3d05a51c6b6c739663d0753aa667fe2
Vin <woqls4@gmail.com>
2022-12-30 15:03:26 +0900

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,8 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
   settings: {
     'import/extensions': ['.js', '.jsx', 'ts', 'tsx'],
